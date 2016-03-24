91cd383ae871e2eb9b93a13697f9bf2760cdf39e
Sebastian Schmidt <isibboi@gmail.com>
2016-03-24 16:33:14 +0100

    Fixed some bugs

--- src/KWayMerge.h
+++ src/KWayMerge.h
@@ -6,10 +6,11 @@
 
 using namespace std;
 
-typedef pair<T , *vector<T> > listhead;
 
 template<typename T>
 class KWayMerge {
+	typedef pair<T , *vector<T> > listhead;
+
     void kway_merge(vector< vector<T> >& sorted_lists, vector<T>& result_to_fill) {
         if ( !result_to_fill.empty() ) {
             throw runtime_error("Result list must be empty");
@@ -24,7 +25,7 @@ class KWayMerge {
         //sort in descending order (not as desired) and reverse result in the end. Easier to implement using vectors
         //Expect the sorted_lists in ascending order
         priority_queue< listhead > pq;
-        for ( int i = 0; i < sorted_lists.size() ) {
+        for (size_t i = 0; i < sorted_lists.size(); i++) {
             if ( sorted_lists[i].size() > 0 ) {
                 pq.emplace( listhead( sorted_lists[i].back(), &sorted_lists[i] ) );
             }
@@ -35,17 +36,17 @@ class KWayMerge {
             pq.pop();
             
             T cur = lh.first;
-            *vector<T> cur_list = lh.second;
+            vector<T> *cur_list = lh.second;
             
             //insert into result
             result_to_fill.push_back( cur );
             
             //chop off the highest element from the current list
-            cur_list.pop_back();
+            cur_list->pop_back();
             
             //reinsert into priority_queue ( equals a decreasekey + list mod )
-            if ( cur_list.size() > 0 ) {
-                lh.first = cur_list.back();
+            if ( cur_list->size() > 0 ) {
+                lh.first = cur_list->back();
                 pq.push( lh );
             }
         }
